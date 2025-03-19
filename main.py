65c3787bf21b7f707791237f0ccda9cdca2c5437
Matteo Martinelli <wmatte2005@gmail.com>
2025-03-19 18:01:26 +0100

    Update main.py
    Updated for tests

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ if __name__ == '__main__':
     my_library = Library('Babilonia', 'Scandiano')
     my_library.load_book_collection_from_file()
     print(my_library)
-    book_searched = my_library.search_book_by_name('Guida Galattica per autostoppisti')
-    print(book_searched)
+    print('\n')
+
+    my_library.lend_book('Nicolò', 'Harry Potter', '34/23/2048', '0/0/0')
+    print(my_library)
+    print('\n')
+
+    my_library.return_book('Harry Potter')
+    print(my_library)
+    print('\n')
+
