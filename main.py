ce594d4a7a2b7a07311fa3526f838dabf435c090
Matteo Martinelli <wmatte2005@gmail.com>
2025-03-19 17:38:03 +0100

    Update main.py
    Search and lend book methods implemented

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     my_library = Library('Babilonia', 'Scandiano')
     my_library.load_book_collection_from_file()
     print(my_library)
-    my_library.write_to_txt_file()
+    book_searched = my_library.search_book_by_name('Guida Galattica per autostoppisti')
+    print(book_searched)
